27877dedbabd5d69359c0d2effacdcc62dd709af
idaNaaber <kevin.randmaa@khk.ee>
2018-11-15 11:16:15 +0200

    Täiendatud ring tüüpi tüübi kirjeldus konstruktoriga, mis võimaldab luua ringi suvalise raadiusega

--- pohimoisted/src/ringtest.java
+++ pohimoisted/src/ringtest.java
@@ -2,5 +2,8 @@ public class ringtest {
     public static void main(String[] args) {
         ring minuring = new ring();
         minuring.pindala();
+        ring punanering = new ring(2.7);
+        punanering.pindala();
+
     }
 }
